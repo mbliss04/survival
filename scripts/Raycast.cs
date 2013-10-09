4994c3c152bc5a8d74d020636d2a82eb520e8f21
McCall Bliss <missmccallkay@gmail.com>
2013-10-10 11:03:39 +1300

    bug fix and color change

--- scripts/Raycast.cs
+++ scripts/Raycast.cs
@@ -2,19 +2,24 @@
  * Raycast.cs
  * Author: McCall Bliss
  * Created: Oct 1, 2013
- * Last Modified: Oct 3, 2013
+ * Last Modified: Oct 10, 2013
  * 
  * Casts a ray from object and, if 
  * the object is close enough and 
  * interactive, it presents a number
  * of different options to the player
  * --------------------------------*/
-
+ 
 using UnityEngine;
 using System.Collections;
 
-public class Raycast : MonoBehaviour {
+//[RequireComponent (typeof (Component<Halo>))]
 
+public class Raycast : MonoBehaviour {
+	
+	// all objects on layer8 (Object layer)
+	int layerObject = 1<<8;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -22,13 +27,25 @@ public class Raycast : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// sends out a ray from the camera
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		Debug.DrawRay(ray.origin, ray.direction*10, Color.cyan);
 		RaycastHit hit;
 		
-		if (Physics.Raycast(ray, out hit, 20)) {
+		// checks if ray hits an object on layer 8
+		if (Physics.Raycast(ray, out hit, 3, layerObject)) {
+			
 			Debug.DrawRay(ray.origin, ray.direction*hit.distance, Color.red);
 			Debug.Log("Ray hit " + hit.transform.gameObject.tag);
+			
+			// change color if has been hit
+			hit.collider.gameObject.renderer.material.color = Color.red;
+			
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				// add to inventory
+			}
 		}
+		
 	}
+	
 }
