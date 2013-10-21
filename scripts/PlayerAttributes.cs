8c8910870862ce0eee3e1a8b0aa889a12ea4ab45
McCall Bliss <missmccallkay@gmail.com>
2013-10-21 23:20:21 +1300

    updates all the bars with an exertion based on character movement

--- scripts/PlayerAttributes.cs
+++ scripts/PlayerAttributes.cs
@@ -7,11 +7,10 @@ public class PlayerAttributes : MonoBehaviour {
 	
 	int numAttr = 6;
 	float maxAmount = 100f;
+	float goingInsane = 40f;
+	float exertion = 0f;
 	
 	float[] attributes;
-
-	bool playerAlive = true;
-	bool playerAsleep = false;
 	
 	// Other objects that effect Attributes
 	public GameObject weather;
@@ -34,88 +33,101 @@ public class PlayerAttributes : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
+		// get the environmental simulator script
+		//EnvironmentalConditionsSimulator weatherCond = weather.gameObject.GetComponent<EnvironmentalConditionsSimulator>();
+		Debug.Log (weather.gameObject.name);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		updateAttributes();
-		checkLifeStatus();
-		if (!playerAlive) {
+		if (!playerAlive()) {
 			endGame();
 		}
 	}
 	
 	// updates all player attributes
 	void updateAttributes() {
+		calcExertion();
 		Hunger ();
 		Warmth ();
 		Sanity ();
 		Thirst ();
 		Energy ();
-		Health ();
+		//Health ();
 	}
 	
 	// updates hunger attribute
 	void Hunger() {
 		
+		float hungerDecrease = 0.0005f;
 		// decreases hunger naturally during day
-		decrease(attr.hunger, 0.0005f);
+		decrease(attr.hunger, (hungerDecrease + exertion));
+	
+	}
+	
+	public void affectHunger(float amount) {
+		
+		attributes[(int)attr.hunger] += amount;
+		attributes[(int)attr.energy] += amount;
 	
 	}
 	
 	// updates warmth attribute
 	void Warmth() {
 		
-		float amount = 0f;
+		float warmthDecrease = 0f;
 		
-		// if night time
+		//if (weather.isDayTime) {
+		
+		//}
 			
-		decrease(attr.warmth, amount);
+		decrease(attr.warmth, warmthDecrease);
 
 	}
 	
 	// updates sanity attribute
 	void Sanity() {
 		
-		// decreases sanity naturally during day
-		decrease(attr.sanity, 0.000005f);
+		float sanityDecrease;
+		
+		// if other attributes are less than certain amount, make
+		// sanity go down faster
+		if (attributes[(int)attr.thirst] < goingInsane ||
+			attributes[(int)attr.hunger] < goingInsane ||
+			attributes[(int)attr.health] < goingInsane) {
+			sanityDecrease = 0.005f;
+		}		
+		else {
+			sanityDecrease = 0.000005f;
+		}
+		
+		decrease(attr.sanity, (sanityDecrease + exertion));
 
 	}
 	
 	// updates thirst attribute
 	void Thirst() {
 		
-		// decreases thirst naturally during day
-		decrease(attr.thirst, 0.005f);
+		float thirstDecrease = 0.005f;
+		decrease(attr.thirst, (thirstDecrease + exertion));
 
+	}
+	
+	public void affectThirst(float amount) {
+	
+		attributes[(int)attr.thirst] += amount;
+		attributes[(int)attr.energy] += amount;
+		
 	}
 
 	// updates energy attribute
 	void Energy() {
 		
-		// if the character jumps, decrease energy by more
-		if (movement.IsJumping()) {
-			decrease(attr.energy, 0.05f);
-		}
+		float energyDecrease = 0.0005f;
+		decrease(attr.energy, (energyDecrease + exertion));
 		
-		// decreases energy naturally during the day if player is not sleeping
-		if (!playerAsleep) {
-			decrease(attr.energy, 0.0005f);
-		}
-		else {
-			while (attributes[(int)attr.energy] < maxAmount) {
-				decrease(attr.energy, -0.005f);
-			}
-		}
-		
-	}
-	
-	// updates health attribute
-	void Health() {
-		
-		
-
 	}
 	
 	// allows other scripts to affect the health condition
@@ -123,6 +135,22 @@ public class PlayerAttributes : MonoBehaviour {
 		
 		decrease(attr.health, increment);
 	
+	}
+	
+	// calculates players exertion and modifies exertion variable
+	private float calcExertion() {
+		
+		float exertion = 0f;
+		
+		if (movement.IsJumping()) {
+			exertion += 0.5f;
+		}
+		if (movement.GetVelocity().magnitude > 0) {
+			exertion += 0.5f;
+		}
+		
+		return exertion;
+		
 	}
 	
 	// decreases each attribute a bit with the passage of time
@@ -133,14 +161,17 @@ public class PlayerAttributes : MonoBehaviour {
 	}
 	
 	// changes life status based on attribute levels
-	void checkLifeStatus() {
+	public bool playerAlive() {
 	
 		// determines if player is dead
 		for (int i = 0; i < numAttr; i++) {
-			if (attributes[i] < 0) {
-				playerAlive = false;
+			if (attributes[i] <= 0) {
+				return false;
 			}
 		}
+		
+		// player is still in the game
+		return true;
 		
 	}
 	
