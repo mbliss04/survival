47686754b639ebb17ea8d10932507f2d449e5f56
McCall Bliss <missmccallkay@gmail.com>
2013-11-03 17:36:29 +1300

    change harvesting to be sent to inventory script

--- scripts/Raycast.cs
+++ scripts/Raycast.cs
@@ -13,8 +13,6 @@
 using UnityEngine;
 using System.Collections;
 
-//[RequireComponent (typeof (Component<Halo>))]
-
 public class Raycast : MonoBehaviour {
 	
 	// all objects on layer8 (Object layer)
@@ -22,31 +20,54 @@ public class Raycast : MonoBehaviour {
 	private int food, harvestedTimes, rayCastDistance;
 	private int berries, grapes, pGrapes, pBerries;
 	private string objectName = "";
+	Inventory playerInv;
 	
 	// Use this for initialization
 	void Start () {
-		rayCastDistance 		= 4;
+		playerInv = gameObject.GetComponent<Inventory>();
+		//toggleMouseVisibility 	= true;
+		rayCastDistance 		= 3;
+		
+		// Lock the cursor at the center of the screen to simulate a gunpoint.
+		//Screen.lockCursor 		= toggleMouseVisibility;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		objectName = "";
-
+		
+		// Toggle mouse visibility if needed.
+		//if (Input.GetKeyDown(KeyCode.Escape)){
+    	//	toggleMouseVisibility = !toggleMouseVisibility;
+			
+		//	Screen.lockCursor = true;//toggleMouseVisibility;
+		//}
+		
+		if (Input.GetKey(KeyCode.Escape))
+			Screen.lockCursor = false;
+		else
+   		 	Screen.lockCursor = true;
+		
+		UnityEngine.
+	
 		// sends out a ray from the camera
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 			
 		// checks if ray hits an object on layer 8
-		if (Physics.Raycast(ray, out hit, rayCastDistance)) {
+		if (Physics.Raycast(ray, out hit, rayCastDistance, 1<<8)) {
 			objectName = hit.transform.gameObject.name;
 			GameObject c = hit.transform.gameObject;
 			
 			if (Input.GetKeyDown(KeyCode.Mouse0)) {
-				
-				// Add to inventory
-				if(objectName.Contains("harvestable"))
+				if(objectName.Contains("food"))
 				{
-					harvestFood(c.GetComponent<Harvest>().getHarvestType(), c.GetComponent<Harvest>().getHarvest());
+					// get name of item hit
+					string typeOfFood = c.GetComponent<Harvest>().getName();
+					float amount = (float)c.GetComponent<Harvest>().getHarvest();
+					
+					// add to inventory
+					playerInv.addFood(typeOfFood, amount);
 					
 					// Now destroy the object.
 					Destroy(c);
